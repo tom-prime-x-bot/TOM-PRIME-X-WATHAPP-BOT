237f0709e38abf24ef25e34346e7797d4a9e28a0
TOM-WATSAPP-BOT-main <sa1986zihad71@gmail.com>
2026-07-22 22:11:16 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "",
+    BOT_NAME: "TOM-PRIME-X",
+    OWNER_NUMBER: "8801XXXXXXXXX", // tmr number dao
+    PREFIX: ".",
+    PORT: process.env.PORT || 3000
+}
